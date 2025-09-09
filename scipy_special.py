b651c35cd597e9b684f0deeced88c6a3d773d231
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-09-09 16:49:46 +0530

    permutation and combinations

--- scipy_special.py
+++ scipy_special.py
@@ -8,3 +8,12 @@ from scipy import special
 import numpy as np
 #compute cube root of each elements
 print("Cube root of elements[64,125,729]: ",special.cbrt(np.array([64,125,729])))
+print("Comput 10**x: ", special.exp10([3,5]))
+print('''Combinations: special.comb(n,m), where
+      n = no of objects
+      m = no of object picked(order doesn't matters)''')
+print("combinations:", special.comb(10,3) )
+print('''permutations: special.perm(n,m)
+      n = no of objects
+      m = no of object picked (order matters)''')
+print("permutations:", special.perm(10,4) )
